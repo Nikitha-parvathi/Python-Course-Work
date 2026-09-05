74a4337276464a8bf4abd77ab0c3d61cb70eb129
Nikitha Parvathi <nikithaparvathi1@gmail.com>
2026-09-05 17:27:49 +0530

    Delete DAY-20 directory

--- DAY-20/main.py
+++
@@ -1,5 +0,0 @@
-import calculator
-print("Addition:", calculator.add(10,5))
-print("subtraction:",calculator.sub(10,5))
-print("Multiplication:", calculator.mul(10,5))
-print("Division:", calculator.div(10,5))
